552adfcfd025aa99dfeab81671a0a3fc686c70cd
Larry Botha <lawrenceguy@gmail.com>
2023-06-07 21:59:25 -0500

    feat(02-01): complete kata

--- katas/src/day1/LinearSearchList.ts
+++ katas/src/day1/LinearSearchList.ts
@@ -1,3 +1,13 @@
 export default function linear_search(haystack: number[], needle: number): boolean {
+    let result = false;
+    const num_values =  haystack.length;
 
+ for (let i = 0; i < num_values; i++) {
+    if (needle == haystack[i]) {
+        result = true
+        break;
+    }
+ }
+
+ return result
 }
