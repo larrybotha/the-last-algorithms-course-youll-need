ca2a0068c4db7e23e42e5a008e6f3d1bbbfd4597
Larry Botha <lawrenceguy@gmail.com>
2023-06-17 07:56:14 -0500

    feat(02-04): complete TS kata

--- katas/src/day1/BinarySearchList.ts
+++ katas/src/day1/BinarySearchList.ts
@@ -1,3 +1,22 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let result = false;
+    let minIndex = 0
+    let maxIndex = haystack.length; // max-exclusive [min, max)
 
+    do {
+        const midIndex = Math.floor(minIndex + (maxIndex - minIndex) / 2)
+        const midValue = haystack[midIndex]
+
+        if (midValue === needle) {
+            result = true
+            break;
+        } else if (midValue < needle) {
+            // exclude the midpoint from subsequent iterations
+            minIndex = midIndex + 1;
+        } else {
+            maxIndex = midIndex
+        }
+    } while (minIndex < maxIndex);
+
+    return result
 }
