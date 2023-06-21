311f4aa379d4f886ab4adbd74c39108d5e5b1181
Larry Botha <lawrenceguy@gmail.com>
2023-06-21 16:23:25 -0500

    style(TwoCrystalBalls.ts): autoformat

--- katas/src/day1/TwoCrystalBalls.ts
+++ katas/src/day1/TwoCrystalBalls.ts
@@ -1,22 +1,22 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-    const rootN = Math.floor(Math.sqrt(breaks.length));
-    let safeIndex = 0;
-    let result = -1;
+  const rootN = Math.floor(Math.sqrt(breaks.length));
+  let safeIndex = 0;
+  let result = -1;
 
-    for (let i = safeIndex; i < breaks.length; i += rootN) {
-        if (breaks[i]) {
-            break;
-        }
-
-        safeIndex = i;
+  for (let i = safeIndex; i < breaks.length; i += rootN) {
+    if (breaks[i]) {
+      break;
     }
 
-    for (let i = safeIndex; i <= safeIndex + rootN; i++) {
-        if (breaks[i]) {
-            result = i;
-            break;
-        }
-    }
+    safeIndex = i;
+  }
 
-    return result;
+  for (let i = safeIndex; i <= safeIndex + rootN; i++) {
+    if (breaks[i]) {
+      result = i;
+      break;
+    }
+  }
+
+  return result;
 }
